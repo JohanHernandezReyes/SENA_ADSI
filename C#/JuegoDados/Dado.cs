ea6c25208571313db7c264ff0ff46f1e26c9ec3b
Johan Hernandez <j.h25@hotmail.com>
2022-05-28 11:52:52 -0500

    Ejercicio juego de dados

---
+++ C#/JuegoDados/Dado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace JuegoDados
+{
+    class Dado
+    {
+        public int numdado;
+
+        public Dado(int n)
+        {
+            numdado = n;
+        }
+
+        public int lanzar()
+        {
+            Console.WriteLine("Lanzando el dado numero " + numdado + "...");
+            Thread.Sleep(1);
+            Random R = new Random();
+            int resultado = R.Next(1, 7);
+            return resultado;
+        }
+    }
+}
---
+++ C#/JuegoDados/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace JuegoDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BIENVENIDO AL JUEGO DE DADOS\n");
+
+            Dado d1 = new Dado(1); int Valord1 = d1.lanzar();
+            Console.WriteLine("Dado 1: " + Valord1+ "\n");
+
+            Dado d2 = new Dado(2); int Valord2 = d2.lanzar();
+            Console.WriteLine("Dado 2: " + Valord2 + "\n");
+
+            Dado d3 = new Dado(3); int Valord3 = d3.lanzar();
+            Console.WriteLine("Dado 3: " + Valord3 + "\n");
+
+            if (Valord1 == Valord2 & Valord1 == Valord3)
+            
+            {Console.WriteLine("\nLos 3 dados son iguales. Felicidades ganaste el juego!!! :)");}
+
+            else if(Valord1 == Valord2 || Valord1 == Valord3 || Valord2 == Valord3)
+            { Console.WriteLine("\nLos valores de 2 dados son iguales. Casi lo logras, sigue intentando!"); }
+
+
+            else
+            { Console.WriteLine("\nLos valores de los 3 dados son diferentes. Perdiste! :(");}
+
+            Console.ReadLine();
+        }
+    }
+}
