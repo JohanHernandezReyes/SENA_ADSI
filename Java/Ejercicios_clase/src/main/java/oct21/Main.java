2aa871b09b133e7092a582ba602e1d365c0922d8
Johan Hernandez <j.h25@hotmail.com>
2022-11-09 22:56:55 -0500

    Correcciones ejercicios

--- Java/Ejercicios_clase/src/main/java/oct21/Main.java
+++ Java/Ejercicios_clase/src/main/java/oct21/Main.java
@@ -8,15 +8,15 @@ public class Main {
     public static void main(String[] args) {
         
         int ejercicio;
-        double num1, num2, num3, num4, num5, resultado;
+        double num1, num2, num3, num4, num5, resultado=0, num;
         DecimalFormat f = new DecimalFormat("####.##");
         
         do{
             ejercicio = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite el numero del ejericio a realizar:\n\n1.Prom de 5 nums\n2.Condicionales\n"
                                                                            + "3.Listar 20 items ingresados\n4.Listar 10 animales y sus  dueños\n5.Dividir 2 números\n"
-                                                                           + "6.Restar 10 a un valor\n7.Multiplicar por 10\n8.Obtener la raiz cuadrada de 3 nums\n9.Simular login Facebook",
+                                                                           + "6.Restar 10 números\n7.Multiplicar por 10\n8.Obtener la raiz cuadrada de 3 nums\n9.Promedio de 15 números\n10.Simular login Facebook",
                                                                            "EJERCICIOS JAVA - MENU PRINCIPAL" ,-1));
-        }while (ejercicio > 9);
+        }while (ejercicio > 10);
         
         if(ejercicio==1)
         {
@@ -53,11 +53,11 @@ public class Main {
         {
             String lista="1. ", animal, dueño;
             
-            for (int i=1; i<=10;i++)
+            for (int i=1; i<=3;i++)
             {
                 animal= JOptionPane.showInputDialog(null,"Ingrese el nombre del animal N°" + i, "INGRESE SUS DATOS", -1);
                 dueño= JOptionPane.showInputDialog(null,"Ingrese el nombre del dueño del animal N°" + i, "INGRESE SUS DATOS", -1);
-                if(i<10){lista+="Animal: "+animal+" - Dueño: "+dueño+"\n"+(i+1)+". ";}
+                if(i<3){lista+="Animal: "+animal+" - Dueño: "+dueño+"\n"+(i+1)+". ";}
                 else {lista+="Animal: "+animal+" - Dueño: "+dueño;}
             }
             
@@ -74,10 +74,13 @@ public class Main {
         }
         
         else if(ejercicio==6)
-        {    
-            num1=Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese un numero"));
-            resultado = num1 - 10;
-            JOptionPane.showMessageDialog(null,num1+" - 10 "+" = "+resultado, "RESULTADO",1);
+        {
+            resultado=Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese el primer número"));
+            for (int i=1; i<=9;i++){
+                num=Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese el número N°"+i+1));
+                resultado-=num;
+            }
+            JOptionPane.showMessageDialog(null,"El resultado de restar los 10 números es: "+resultado, "RESULTADO",1);
         }
         
         else if(ejercicio==7)
@@ -100,11 +103,27 @@ public class Main {
                                               + "\nLa raiz cuadrada de "+ num2 + " = " +f.format(num4)
                                               + "\nLa raiz cuadrada de "+ num3 + " = " +f.format(num5)  , "RESULTADO",1);
         }
-        
-        else if(ejercicio==9)
+
+        else if(ejercicio==9) {
+            for (int i=1; i<=15;i++){
+                num=Double.parseDouble(JOptionPane.showInputDialog(null,"Ingrese el número N°"+i));
+                resultado+=num;
+            }
+            resultado/=15;
+            JOptionPane.showMessageDialog(null,"El promedio de los 15 números ingresados es " + f.format(resultado) , "Promedio",1);
+        }
+
+        else if(ejercicio==10)
         {   
             String user;
-            user = JOptionPane.showInputDialog(null,"Ingrese su email", "LOGIN", -1);
+            do{
+                user = JOptionPane.showInputDialog(null,"Ingrese su email", "LOGIN", -1);
+                if (user.matches(".*[@)].*[.com].*")==false){
+                    JOptionPane.showMessageDialog(null,"El email no es válido", "Error", 0);
+                };
+            } while (user.matches(".*[@)].*[.com].*")==false);
+
+
             JOptionPane.showInputDialog(null,"Ingrese su password", "LOGIN", -1);
             JOptionPane.showMessageDialog(null,"Ha iniciado sesion en Facebook\nBienvenido " + user , "SIGN IN",1);
         }
