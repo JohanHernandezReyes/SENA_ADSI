350b2bc19646f265f48fa231460aab3e00bfd837
Johan Hernandez <j.h25@hotmail.com>
2023-04-04 19:44:58 -0500

    Proyecto Peluqueria Canina JPA

--- Java/PeluqueriaCanina/target/generated-sources/annotations/PeluqueriaCanina/Logica/Duenio_.java
+++ Java/PeluqueriaCanina/target/generated-sources/annotations/PeluqueriaCanina/Logica/Duenio_.java
@@ -4,7 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-27T01:21:27", comments="EclipseLink-2.7.7.v20200504-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-03-28T21:34:09", comments="EclipseLink-2.7.7.v20200504-rNA")
 @StaticMetamodel(Duenio.class)
 public class Duenio_ { 
 
